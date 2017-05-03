a40bd9855a1858aae83f165e6c0416d5c3f643c8
Howard Dutton <hjd1964@gmail.com>
2017-05-03 06:56:45 -0400

    Improved comment

--- Config.h
+++ Config.h
@@ -8,8 +8,8 @@
 #define SERIAL_BAUD ":SB1#" // 0=115.2K, 1=57.6K, 2=38.4K Baud, 3=28.8K, 4=19.2K Baud ... Default ":SB1#", if talking to a Mega2560 ":SB4#"
 #define SERIAL_SWAP_OFF     // Default=OFF, ON to swap ESP8266 Serial default location GPIO 1(TX)/3(RX) to 15(TX)/13(RX) useful for WeMos D1 Mini, for example
 
-// The settings below are for initialization only, afterward they are stored and recalled from using EEPROM and must be changed
-// in the web interface OR with a reset (for initialization again) as described below
+// The settings below are for initialization only, afterward they are stored and recalled from EEPROM and must
+// be changed in the web interface OR with a reset (for initialization again) as described in the comments below
 #define TIMEOUT_WEB 15      // Default=15, if talking to a Mega2560 double the default
 #define TIMEOUT_CMD 30      // Default=30, if talking to a Mega2560 double the default
 
